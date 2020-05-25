089826df478e363ce4e5a98857d3377c22da45b2
callas4ever <thetseringnorbu@gmail.com>
2020-05-24 23:46:39 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,21 @@
 class Actor < ActiveRecord::Base
   
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+      "#{self.first_name} #{self.last_name}"
+    end
+    
+    def list_roles
+        
+        Character.all.each do |ch|
+            if ch.actor==self
+                return "#{ch.name} - #{ch.show.name}"
+            end
+        end
+        
+
+    end
+    
 end
